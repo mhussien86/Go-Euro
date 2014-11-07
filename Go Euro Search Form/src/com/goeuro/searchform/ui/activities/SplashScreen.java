4182586bbc219ca564dccb0cb77771bdd3e68bd4
Mohamed Hussein <mohamed.hussein-khattab@vodafone.com.eg>
2014-11-07 02:50:07 +0200

    Adding splash activity timer

--- Go Euro Search Form/src/com/goeuro/searchform/ui/activities/SplashScreen.java
+++ Go Euro Search Form/src/com/goeuro/searchform/ui/activities/SplashScreen.java
@@ -1,20 +1,38 @@
 package com.goeuro.searchform.ui.activities;
 
 import com.goeuro.searchform.R;
+import com.goeuro.searchform.common.constants.AppConstants;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class SplashScreen extends Activity {
 
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         
-        
+        new Handler().postDelayed(new Runnable() {
+        	 
+ 
+ 
+            @Override
+            public void run() {
+     
+                Intent i = new Intent(SplashScreen.this, SearchFormActivity.class);
+                startActivity(i);
+ 
+             
+                finish();
+            }
+        }, AppConstants.SPLASH_SCREEN_TIME_OUT);
         
         
     }
