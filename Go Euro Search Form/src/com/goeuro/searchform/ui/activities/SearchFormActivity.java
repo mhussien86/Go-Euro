8e0330c0eb8a2914a96b1ffd423c6344c65440a4
Mohamed Hussein <mohamed.hussein-khattab@vodafone.com.eg>
2014-11-16 19:08:57 +0200

    Adding preogress generator interface and search strings

--- Go Euro Search Form/src/com/goeuro/searchform/ui/activities/SearchFormActivity.java
+++ Go Euro Search Form/src/com/goeuro/searchform/ui/activities/SearchFormActivity.java
@@ -1,12 +1,15 @@
 package com.goeuro.searchform.ui.activities;
 
+import com.dd.processbutton.iml.ActionProcessButton;
 import com.goeuro.searchform.R;
+import com.goeuro.searchform.common.util.ProgressGenerator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 
-public class SearchFormActivity extends Activity{
+public class SearchFormActivity extends Activity implements ProgressGenerator.OnCompleteListener {
 
 	
 	
@@ -15,6 +18,33 @@ public class SearchFormActivity extends Activity{
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search_form);
+		initUI();
+		
+	}
+	
+	
+	public void initUI(){
+		
+		final ProgressGenerator progressGenerator = new ProgressGenerator(this);
+		final ActionProcessButton btnSignIn = (ActionProcessButton) findViewById(R.id.search_btn);
+		btnSignIn.setMode(ActionProcessButton.Mode.PROGRESS);
+		btnSignIn.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				progressGenerator.start(btnSignIn);
+				btnSignIn.setEnabled(false);
+
+			}
+		});
+	}
+
+
+	@Override
+	public void onComplete() {
+		// TODO Auto-generated method stub
+		
 		
 		
 	}
